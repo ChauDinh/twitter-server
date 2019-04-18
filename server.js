4e95ef1100b782b8aa1175216029feb67f3c2df4
Chau Dinh <macbookpro@Macbooks-MBP.net.fpt>
2019-04-18 22:27:50 +0700

    cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const users = require("./routes/users");
+const cors = require("cors");
 
 // setup environment variables
 dotenv.config();
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api/users", users);
 
